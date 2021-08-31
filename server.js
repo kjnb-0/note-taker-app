2d57e046380abbf7cdd2eb994983774e6bcb83f0
Krista Nielsen <kristanielsen@Kristas-MacBook-Air.local>
2021-08-30 18:36:56 -0700

    adding app.delete

--- server.js
+++ server.js
@@ -26,25 +26,49 @@ app.get("/api/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "db/db.json"));
 });
 
-
+app.get("/api/notes/:note", function (req, res) {
+  var selectedNote = req.params.note;
+  res.json(selectedNote);
+});
 
 // POST /api/notes
 app.post("/api/notes", (req, res) => {
   const newNote = req.body;
-  // create a unique identifier with Date.now()
+  //adding ID so we can delete later
   newNote.id = Date.now();
   // Add to the db.json file
-  let noteData = fs.readFileSync('./db/db.json');
+  let noteData = fs.readFileSync("./db/db.json");
   let noteTaker = JSON.parse(noteData);
   // Push to array
   noteTaker.push(req.body);
   // Stringify new array
-  fs.writeFileSync('./db/db.json',JSON.stringify(noteTaker), (err, data) => {
+  fs.writeFileSync("./db/db.json", JSON.stringify(noteTaker), (err, data) => {
     if (err) throw err;
-    res.json(noteTaker)      
-  }); 
+    res.json(noteTaker);
+  });
 
-  res.sendFile(path.join(__dirname,'public/notes.html'));
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete.
+
+app.delete("/api/notes/:id", (req, res) => {
+  // Read all notes from db.json file
+  let notedata = fs.readFileSync("./db/db.json");
+  let noteTaker = JSON.parse(notedata);
+  
+  const savedNotes = noteTaker.find((n) => n.id === parseInt(req.params.id));
+  const notesIndex = noteTaker.indexOf(savedNotes);
+  noteTaker.splice(notesIndex);
+
+  fs.writeFile(
+    __dirname + "/db/db.json",
+    JSON.stringify(noteTaker),
+    (err, data) => {
+      if (err) throw err;
+      res.json(noteTaker);
+    }
+  );
 });
 
 // GET /notes
